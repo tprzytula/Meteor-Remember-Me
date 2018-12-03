17d8ca6a810642f4829a845d0519a1a0ab286b87
Tomasz Przytuła <t.przytula.dev@gmail.com>
2018-12-03 16:29:41 +0000

    Add support to accounts that are identified by email instead of username

--- test/server/integration/accounts.spec.js
+++ test/server/integration/accounts.spec.js
@@ -4,6 +4,8 @@ import sinon from 'sinon';
 import {
     addValidationStep,
     findUserByUsername,
+    findUserByEmail,
+    findUser,
     getConnectionLastLoginToken,
     hashLoginToken
 } from '../../../server/integration/accounts';
@@ -54,6 +56,88 @@ describe('Given findUserByUsername method', () => {
 
         it('should pass the param to the Accounts findUserByUsername method', () => {
             expect(findUserByUsernameStub).to.be.calledWith(sampleUsername);
+        });
+    });
+});
+
+describe('Given findUserByEmail method', () => {
+    const sandbox = sinon.createSandbox();
+    let findUserByEmailStub;
+
+    beforeEach(() => {
+        findUserByEmailStub = sandbox.stub(Accounts, 'findUserByEmail');
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('When a param is passed', () => {
+        const sampleEmail = 'test@test.com';
+
+        beforeEach(() => {
+            findUserByEmail(sampleEmail);
+        });
+
+        it('should pass the param to the Accounts findUserByEmail method', () => {
+            expect(findUserByEmailStub).to.be.calledWith(sampleEmail);
+        });
+    });
+});
+
+describe('Given findUser method', () => {
+    const sandbox = sinon.createSandbox();
+    const sampleUsername = 'test';
+    const sampleEmail = 'test@test.com';
+    let findUserByUsernameStub, findUserByEmailStub;
+
+    beforeEach(() => {
+        findUserByUsernameStub = sandbox.stub(Accounts, 'findUserByUsername');
+        findUserByEmailStub = sandbox.stub(Accounts, 'findUserByEmail');
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('When the record contains username and email', () => {
+        beforeEach(() => {
+            findUser({ username: sampleUsername, emails: [ { address: sampleEmail } ] });
+        });
+
+        it('should pass the username to the Accounts findUserByUsernameStub method', () => {
+            expect(findUserByUsernameStub).to.be.calledWith(sampleUsername);
+        });
+    });
+
+    describe('When the record contains only username', () => {
+        beforeEach(() => {
+            findUser({ username: sampleUsername });
+        });
+
+        it('should pass the username to the Accounts findUserByUsernameStub method', () => {
+            expect(findUserByUsernameStub).to.be.calledWith(sampleUsername);
+        });
+    });
+
+    describe('When the record contains only email', () => {
+        beforeEach(() => {
+            findUser({ emails: [ { address: sampleEmail } ] });
+        });
+
+        it('should pass the email to the Accounts findUserByEmail method', () => {
+            expect(findUserByEmailStub).to.be.calledWith(sampleEmail);
+        });
+    });
+
+    describe('When the record does not contain username or email', () => {
+        beforeEach(() => {
+            findUser();
+        });
+
+        it('should not call the Accounts methods', () => {
+            expect(findUserByUsernameStub).to.not.be.called();
+            expect(findUserByEmailStub).to.not.be.called();
         });
     });
 });
